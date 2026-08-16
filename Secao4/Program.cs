7bbd2a1a44bd5b56d09dc43630a6c4e288da20d1
ihpb22 <ivopedrini97@gmail.com>
2026-08-16 19:55:15 -0300

    Seção 4: Resolução de Exercicios com Classe v3.

--- Secao4/Program.cs
+++ Secao4/Program.cs
@@ -3,47 +3,6 @@ using System.Globalization;
 
 namespace Secao4{
 
-    class Triangulo {
-        private double ladoX;
-        private double ladoY;
-        private double ladoZ;
-
-        public Triangulo(double x, double y, double z){
-            this.setX(x);
-            this.setY(y);
-            this.setZ(z);
-        }
-        
-        private void setX(double x){
-            this.ladoX = x;
-        }
-
-        private void setY(double y){
-            this.ladoY = y;
-        }
-
-        private void setZ(double z){
-            this.ladoZ = z;
-        }
-
-        public double getX(){
-            return this.ladoX;
-        }
-
-        public double getY(){
-            return this.ladoY;
-        }
-
-        public double getZ(){
-            return this.ladoZ;
-        }
-
-        public double CalcArea (){
-            double p = (this.getX() + this.getY() + this.getZ())/2;
-            return Math.Sqrt(p*(p-this.getX())*(p-this.getY())*(p-this.getZ()));
-        }
-    }
-
     class Program {
 
         private static List<double> toDouble(string[] argsString){
