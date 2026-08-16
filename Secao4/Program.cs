7c6cf950afcea37919a050ebe378b36dafa08b82
ihpb22 <ivopedrini97@gmail.com>
2026-08-16 19:10:44 -0300

    Seção 4: Resolução de Exercicios com Classe v2.

--- Secao4/Program.cs
+++ Secao4/Program.cs
@@ -4,25 +4,49 @@ using System.Globalization;
 namespace Secao4{
 
     class Triangulo {
-        public double ladoX;
-        public double ladoY;
-        public double ladoZ;
+        private double ladoX;
+        private double ladoY;
+        private double ladoZ;
 
         public Triangulo(double x, double y, double z){
-            ladoX = x;
-            ladoY = y;
-            ladoZ = z;
+            this.setX(x);
+            this.setY(y);
+            this.setZ(z);
         }
         
+        private void setX(double x){
+            this.ladoX = x;
+        }
+
+        private void setY(double y){
+            this.ladoY = y;
+        }
+
+        private void setZ(double z){
+            this.ladoZ = z;
+        }
+
+        public double getX(){
+            return this.ladoX;
+        }
+
+        public double getY(){
+            return this.ladoY;
+        }
+
+        public double getZ(){
+            return this.ladoZ;
+        }
+
         public double CalcArea (){
-            double p = (this.ladoX + this.ladoY + this.ladoZ)/2;
-            return Math.Sqrt(p*(p-this.ladoX)*(p-this.ladoY)*(p-this.ladoZ));
+            double p = (this.getX() + this.getY() + this.getZ())/2;
+            return Math.Sqrt(p*(p-this.getX())*(p-this.getY())*(p-this.getZ()));
         }
     }
 
     class Program {
 
-        private List<double> toDouble(string[] argsString){
+        private static List<double> toDouble(string[] argsString){
             List<double> argsDouble = new List<double>(argsString.Length);
             for(int i = 0; i < argsString.Length; i++){
                 try{
@@ -30,31 +54,43 @@ namespace Secao4{
                 }
                 catch (FormatException){
                     Console.WriteLine($"O input '{argsString[i]}' nao e um numero valido.");
+                    break;
                 } 
             }
             return argsDouble;
         }
 
         static void Main(string[] args) {
-            Program k = new Program();
-            List<double> argsDouble = k.toDouble(args);
+            if(args.Length != 6)
+            {
+                Console.WriteLine("Erro de entrada, por favor insira uma entrada correta (QUANTIDADE INSUFICIENTE DE ENTRADAS).\nPrograma finalizando.");
+                return;
+            }
+
+            List<double> argsDouble = toDouble(args);
+            
+            if(argsDouble.Count < args.Length){
+                Console.WriteLine("Erro de entrada, por favor insira uma entrada correta (APENAS NUMEROS).\nPrograma finalizando.");
+                return;
+            }
+            
             Triangulo x = new Triangulo(argsDouble[0], argsDouble[1], argsDouble[2]);
             Triangulo y = new Triangulo(argsDouble[3], argsDouble[4], argsDouble[5]);
+            
             double a = x.CalcArea();
             double b = y.CalcArea();
+            
             Console.WriteLine("AREA DE X = " + a.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("AREA DE Y = " + b.ToString("F4", CultureInfo.InvariantCulture));
+            
             if(a > b) {
                 Console.WriteLine("X E MAIOR.");
-                return;
             }
             if(a < b){
                 Console.WriteLine("Y E MAIOR.");
-                return;
             }
             else{
                 Console.WriteLine("MESMO TAMANHO.");
-                return;
             }
         }
     }
