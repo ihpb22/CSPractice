d07b6eeb008c2eb3d1ea4064d61a958c21e229f3
ihpb22 <ivopedrini97@gmail.com>
2026-08-16 18:44:57 -0300

    Seção 4: Resolução de Exercicios com Classe.

--- Secao4/Program.cs
+++ Secao4/Program.cs
@@ -2,12 +2,27 @@
 using System.Globalization;
 
 namespace Secao4{
-    class Aula1 {
-        public Aula1(){
 
+    class Triangulo {
+        public double ladoX;
+        public double ladoY;
+        public double ladoZ;
+
+        public Triangulo(double x, double y, double z){
+            ladoX = x;
+            ladoY = y;
+            ladoZ = z;
         }
         
-        public List<double> toDouble(string[] argsString){
+        public double CalcArea (){
+            double p = (this.ladoX + this.ladoY + this.ladoZ)/2;
+            return Math.Sqrt(p*(p-this.ladoX)*(p-this.ladoY)*(p-this.ladoZ));
+        }
+    }
+
+    class Program {
+
+        private List<double> toDouble(string[] argsString){
             List<double> argsDouble = new List<double>(argsString.Length);
             for(int i = 0; i < argsString.Length; i++){
                 try{
@@ -19,34 +34,28 @@ namespace Secao4{
             }
             return argsDouble;
         }
-        
-        public double CalcArea (double x, double y, double z){
-            double p = (x + y + z)/2;
-            return Math.Sqrt(p*(p-x)*(p-y)*(p-z));
-        }
-    }
 
-    class Program {
         static void Main(string[] args) {
-            Aula1 k = new Aula1();
+            Program k = new Program();
             List<double> argsDouble = k.toDouble(args);
-            double a = new Double();
-            double b = new Double();
-            try{
-                a = k.CalcArea(argsDouble[0], argsDouble[1], argsDouble[2]);
-                b = k.CalcArea(argsDouble[3], argsDouble[4], argsDouble[5]);
-            }
-            catch(ArgumentOutOfRangeException){
-                return;
-            }
+            Triangulo x = new Triangulo(argsDouble[0], argsDouble[1], argsDouble[2]);
+            Triangulo y = new Triangulo(argsDouble[3], argsDouble[4], argsDouble[5]);
+            double a = x.CalcArea();
+            double b = y.CalcArea();
             Console.WriteLine("AREA DE X = " + a.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("AREA DE Y = " + b.ToString("F4", CultureInfo.InvariantCulture));
             if(a > b) {
                 Console.WriteLine("X E MAIOR.");
                 return;
             }
-            Console.WriteLine("Y E MAIOR.");
-            return;
+            if(a < b){
+                Console.WriteLine("Y E MAIOR.");
+                return;
+            }
+            else{
+                Console.WriteLine("MESMO TAMANHO.");
+                return;
+            }
         }
     }
 }
--- Secao4/obj/Debug/net10.0/Secao4.AssemblyInfo.cs
+++ Secao4/obj/Debug/net10.0/Secao4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Secao4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c781261da4b51acb36571d7e327cba3d54ba7e47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Secao4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Secao4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
