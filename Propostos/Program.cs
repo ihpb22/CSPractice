180e92ccc5aaf8cdf21647d7a18cf6d2e226320e
ihpb22 <ivopedrini97@gmail.com>
2026-08-18 16:35:55 -0300

    Todos os propostos prontos

--- Propostos/Program.cs
+++ Propostos/Program.cs
@@ -4,14 +4,14 @@ namespace Propostos
 {
     class Program{
         static void Main(string[] args){
-            //Console.WriteLine("Primeiro Exercicio:");
-            //Retangulo ret = new Retangulo();
-            //ret.ReadInput();
-            //ret.WriteCalcs();
+            Console.WriteLine("Primeiro Exercicio:");
+            Retangulo ret = new Retangulo();
+            ret.ReadInput();
+            ret.WriteCalcs();
 
-            //Console.WriteLine("Segundo Exercicio:");
-            //Funcionario worker = new Funcionario();
-            //worker.ReadInput();
+            Console.WriteLine("Segundo Exercicio:");
+            Funcionario worker = new Funcionario();
+            worker.ReadInput();
 
             Console.WriteLine("Terceiro Exercicio:");
             Aluno aluno = new Aluno();
